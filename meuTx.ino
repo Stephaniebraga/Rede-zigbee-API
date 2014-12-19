869f5cb44f1c2422cca7d63aa11c16e5ecf8ddd5
Stephanie Braga <stephanie.abraga@gmail.com>
2014-12-19 09:55:00 -0300

    payload grande

--- meuTx.ino
+++ meuTx.ino
@@ -1,10 +1,10 @@
 #include <XBee.h>
 
 XBee xbee = XBee();
-uint8_t payload=4;
+uint8_t payload[]={0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19};
 
 XBeeAddress64 addr64 = XBeeAddress64(0x0013a200, 0x408a30a2);
-ZBTxRequest zbTx = ZBTxRequest(addr64, &payload , sizeof(payload)) ;
+ZBTxRequest zbTx = ZBTxRequest(addr64, payload , sizeof(payload)) ;
 ZBTxStatusResponse txStatus = ZBTxStatusResponse();
 
 void setup() {
